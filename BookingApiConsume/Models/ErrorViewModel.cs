dc39fb8422e78d43f80cd7ab6d22a8dbad3af4e3
edaagunes <edagunes00@hotmail.com>
2024-12-24 02:01:25 +0300

    Add project files.

---
+++ BookingApiConsume/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookingApiConsume.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
