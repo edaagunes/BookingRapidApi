f1d7b70d223e132ce4e39c1db9dc2b4a53294b26
edaagunes <edagunes00@hotmail.com>
2024-12-24 02:30:27 +0300

    Added theme and edited

---
+++ BookingApiConsume/ViewComponents/Layout/_LayoutBanner.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApiConsume.ViewComponents.Layout
+{
+	public class _LayoutBanner:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ BookingApiConsume/ViewComponents/Layout/_LayoutHead.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApiConsume.ViewComponents.Layout
+{
+	public class _LayoutHead:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
